4708fdf422adec198a53ec891da98a2da103f10f
BENHEL123 <dimokrazanchik@gmail.com>
2025-10-01 18:58:23 +0400

    Create task 2.2.py

---
+++ task 2.2.py
@@ -0,0 +1,71 @@
+# здесь подключаются модули
+import pygame as pg
+
+# здесь определяются константы, функции и классы
+FPS = 120
+WIDTH = 1000
+HEIGHT = 600
+SPEED = 10
+WHITE = (255, 255, 255)
+GRAY = (110, 110, 110)
+ORANGE = (255, 128, 0)
+# здесь происходит инициализация, создание объектов
+pg.init()
+screen = pg.display.set_mode((WIDTH, HEIGHT))  # также здесь можно указать битовые флаги
+pg.display.set_caption("Игра")
+clock = pg.time.Clock()
+screen.fill(GRAY)
+
+r_x = 100
+r_y = 75
+x = 0
+y = 0
+flag_x = True
+flag_y = False
+rflag_x = False
+rflag_y = False
+pg.draw.ellipse(screen, ORANGE, (x,y,r_x,r_y))
+# если надо до цикла отобразить какие-то объекты, обновляем экран
+pg.display.update()
+
+# главный цикл
+flag_play = True
+while flag_play:
+    # настраиваем частоту итераций в секунду
+    clock.tick(FPS)
+
+    # цикл обработки событий
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            flag_play = False
+            break
+    if not flag_play:
+        break
+
+    # изменение объектов
+    if x >= WIDTH - r_x and y <= 0:
+        flag_x = False
+        flag_y = True
+    elif x >= WIDTH - r_x and y >= HEIGHT - r_y:
+        flag_y = False
+        rflag_x = True
+    elif x <= 0 and y >= HEIGHT - r_y:
+        rflag_x = False
+        rflag_y = True
+    elif x <= 0 and y <= 0:
+        rflag_y = False
+        flag_x = True
+
+    if flag_y:
+        y += SPEED
+    elif flag_x:
+        x += SPEED
+    elif rflag_x:
+        x -= SPEED
+    elif rflag_y:
+        y -= SPEED
+
+    screen.fill(GRAY)
+    pg.draw.ellipse(screen, ORANGE, (x, y, r_x, r_y))
+    pg.display.update()
