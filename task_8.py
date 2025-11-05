c433b91200ac4d316ea1910efc760f48240624b8
BENHEL123 <dimokrazanchik@gmail.com>
2025-11-05 19:28:53 +0400

    Create task_8.py

---
+++ task_8.py
@@ -0,0 +1,79 @@
+import pygame as pg
+import random
+class Ball:
+    def __init__(self):
+        self.ball_surf = pg.Surface((BALL_WIDTH, BALL_HEIGTH), pg.SRCALPHA)
+        self.ball_surf.fill((0, 0, 0, 0))
+        pg.draw.circle(self.ball_surf, (*BLUE, 90), (BALL_WIDTH // 2, BALL_HEIGTH // 2), R)
+        self.speed = random.randint(0,15)
+        self.ball_rect = self.ball_surf.get_rect(topleft=(0 - R, H1))
+
+    def move_ball(self):
+        if self.ball_rect.left >= WIDTH and self.ball_rect.top == H1:
+            self.ball_rect.top = H2
+            self.ball_rect.left = WIDTH
+        if self.ball_rect.right <= 0 and self.ball_rect.top == H2:
+            self.ball_rect.top = H1
+            self.ball_rect.left = 0 - BALL_WIDTH
+        if 0 - BALL_WIDTH <= self.ball_rect.left <= WIDTH + BALL_WIDTH and self.ball_rect.top <= 150:
+            self.ball_rect.move_ip(self.speed, 0)
+        if 0 - BALL_WIDTH <= self.ball_rect.left <= WIDTH + BALL_WIDTH and self.ball_rect.top >= 300:
+            self.ball_rect.move_ip(-self.speed, 0)
+        return self.ball_rect
+
+    def draw(self, screen):
+        screen.blit(self.ball_surf, self.ball_rect)
+
+    def get(self):
+        return
+
+    def add(self):
+        return
+
+
+FPS = 60
+WIDTH, HEIGHT = 1000, 600
+LIGHT_BLUE_BLUE = (96, 110, 140)
+LIGHT_PURPLE = (108, 77, 117)
+BLUE = (0, 51, 153)
+R = 50
+BALL_WIDTH = BALL_HEIGTH = R * 2
+H1 = HEIGHT / 6 + 15
+H2 = HEIGHT / 6 + 275 + 15
+
+pg.init()
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+pg.display.set_caption("Игра")
+clock = pg.time.Clock()
+
+background = pg.Surface((WIDTH, HEIGHT))
+background.fill(LIGHT_PURPLE)
+pg.draw.rect(background, LIGHT_BLUE_BLUE, (0, HEIGHT / 6, WIDTH, 130))
+pg.draw.rect(background, LIGHT_BLUE_BLUE, (0, HEIGHT / 6 + 275, WIDTH, 130))
+
+screen.blit(background, (0, 0))
+# screen.blit(ball, ball_rect.topleft)
+# screen.blit(ball, ball2_rect.topleft)
+pg.display.update()
+Balls = []
+flag_play = True
+while flag_play:
+    clock.tick(FPS)
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            flag_play = False
+            break
+    if not flag_play:
+        break
+    Balls.append(Ball.move_ball())
+
+
+    # ball_rect = move_ball(ball_rect, SPEED)
+    # ball2_rect = move_ball(ball2_rect, SPEED + 5)
+
+    screen.blit(background, (0, 0))
+    # screen.blit(ball, ball_rect.topleft)
+    # screen.blit(ball, ball2_rect.topleft)
+    pg.display.update()
