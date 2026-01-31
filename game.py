d8c480638d82a96ac2dd4a48a76b88fd2de4f033
BENHEL123 <dimokrazanchik@gmail.com>
2026-01-31 19:34:18 +0400

    Update game.py

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 # здесь подключаются модули:
 import pygame as pg
 import random
+import time
+import asyncio
 # здесь определяются константы, функции и классы:
 FPS = 60
 WIDTH, HEIGHT = 1280, 900
@@ -12,7 +14,7 @@ BLACK = (0, 0, 0)
 
 class SpaceShip(pg.sprite.Sprite):
     def __init__(self):
-        self.hp_max = 5
+        self.hp_max = 20
         self.hp = self.hp_max
         self.image = pg.image.load(r'imagesgame/spaceship.png')
         self.image = pg.transform.scale(self.image, (150, 150))
@@ -30,7 +32,7 @@ class SpaceShip(pg.sprite.Sprite):
 
 class Planet(pg.sprite.Sprite):
     def __init__(self):
-        super().__init__()
+        pg.sprite.Sprite.__init__(self)
         self.image = pg.image.load(r'imagesgame/planet.png').convert_alpha()
         self.image = pg.transform.scale(self.image, (220, 220))
         self.rect = self.image.get_rect()
@@ -63,7 +65,7 @@ class Asteroids(pg.sprite.Sprite):
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
         num = random.randint(1,5)
-        asteroid_scale = random.randint(200, 500)
+        asteroid_scale = random.randint(200, 400)
         self.image = pg.image. load(r'imagesgame/asteroid' + str(num) + '.png')
         self.image = pg.transform.scale(self.image, (asteroid_scale, asteroid_scale))
         self.rect = self.image.get_rect()
@@ -105,20 +107,29 @@ class Background:
         screen.blit(self.image, (self.x2, 0))
         screen.blit(self.image, (self.x3, 0))
 
+def draw_ui(screen, money, deliveries):
+    font = pg.font.Font(None, 48)
+
+    money_text = font.render(f"Money: {money}$", True, WHITE)
+    screen.blit(money_text, (WIDTH - 300, 20))
+
+    deliveries_text = font.render(f"Deliveries: {deliveries}", True, WHITE)
+    screen.blit(deliveries_text, (WIDTH - 300, 50))
+
 def draw_hp(screen, hp, hp_max):
-    font = pg.font.Font(None, 48)  # встроенный шрифт
-    text = font.render("HP:", True, BLACK)
+    font = pg.font.Font(None, 48)
+    text = font.render("HP:", True, RED)
     screen.blit(text, (20, 20))
 
     size = 25
-    gap = 8
+    a = 8
     start_x = 20 + text.get_width() + 15
     y = 25
 
     for i in range(hp_max):
         color = RED if i < hp else GRAY
-        pg.draw.rect(screen, color, (start_x + i * (size + gap), y, size, size), 0)  # width=0 => заливка [web:58]
-        pg.draw.rect(screen, BLACK, (start_x + i * (size + gap), y, size, size), 2)
+        pg.draw.rect(screen, color, (start_x + i * (size + a), y, size, size), 0)
+        pg.draw.rect(screen, BLACK, (start_x + i * (size + a), y, size, size), 2)
 
 # здесь происходит инициализация:
 pg.init()
@@ -128,21 +139,26 @@ clock = pg.time.Clock()
 background = Background()
 # здесь происходит создание игровых объектов:
 # ...
+font1 = pg.font.Font(None, 96)
 asteroids = pg.sprite.Group()
 spaceship = SpaceShip()
 planets = pg.sprite.Group()
+money = 0
+deliveries = 0
+cooldown = 0
+font_ui = pg.font.Font(None, 36)
 # если надо до игрового цикла (=на самом старте игры) отобразить объекты, то отрисовываем их здесь:
 # ...
 pg.display.update()  # затем обновляем экран, чтобы показать изменения
 spawn_asteroid = pg.USEREVENT + 1
 SPAWN_PLANET = pg.USEREVENT + 2
-pg.time.set_timer(spawn_asteroid, 300)
+pg.time.set_timer(spawn_asteroid, 600)
 pg.time.set_timer(SPAWN_PLANET, 3500)
 # главный игровой цикл:
 flag_play = True
 while flag_play:
     clock.tick(FPS)  # настраиваем FPS (=частоту итераций в секунду)
-
+    cooldown += 1
 
     # цикл обработки событий:
     for event in pg.event.get():
@@ -181,12 +197,28 @@ while flag_play:
     asteroids.draw(screen)
     draw_hp(screen, spaceship.hp, spaceship.hp_max)
     if pg.sprite.spritecollideany(spaceship, asteroids, collided=pg.sprite.collide_mask):
-        spaceship.hp -= 1
+        if cooldown >= 30:
+            # explosion(x,y)
+            cooldown = 0
+            spaceship.hp -= 1
         if spaceship.hp <= 0:
             break
     for planet in planets:
         planet.move()
         if planet.try_deliver(spaceship.rect):
+            deliveries += 1
+            salary = random.randint(20, 80)
+            chance = random.randint(1,25)
+            if chance == 20:
+                salary += random.randint(200, 500)
+                text = font1.render("Big Tips", True, WHITE)
+                screen.blit(text, (300, 300))
+            if chance > 20:
+                salary += random.randint(20,50)
+                text = font1.render("Tips", True, WHITE)
+                screen.blit(text, (300, 300))
+            money += salary
             # count of delivery and money
             pass
+        draw_ui(screen, money, deliveries)
     pg.display.update()  # обновление экрана, чтобы отобразить новую перерисовку
